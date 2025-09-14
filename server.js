be08c03b36d3ae3062f17291ed94582a9b032133
Davis <kingdavidcyril102@gmail.com>
2025-09-14 05:47:26 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+  target: 'http://206.189.146.215:1816/',
+  changeOrigin: true,
+  secure: false
+}));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Proxy running on port ${PORT}`);
+});
